2af1ab245fceacfc2afb6b4f1d7dfbdc7ab3f3c5
kawsar007 <imkawsar007@gmail.com>
2022-06-16 13:21:15 +0600

    initial commit

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit } from '@angular/core';
+// import { MatDialog } from '@angular/material/dialog';
+// import { NgToastService } from 'ng-angular-popup';
+// import { ApiService } from '../services/api.service';
+// import {MatTableDataSource} from '@angular/material/table';
+// import {MatPaginator} from '@angular/material/paginator';
+// import { MatSort } from '@angular/material/sort';
+// import {AfterViewInit, ViewChild} from '@angular/core';
+// import { DialogComponent } from '../dialog/dialog.component';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
+})
+export class AdminComponent implements OnInit {
+
+  // displayedColumns: string[] = ['productName', 'category', 'date', 'freshness', 'price', 'comment', 'action'];
+  // dataSource !:  MatTableDataSource<any>;
+
+  // @ViewChild(MatPaginator) paginator !: MatPaginator;
+  // @ViewChild(MatSort) sort !: MatSort;
+
+  // constructor(private dialog: MatDialog, private api : ApiService, private toast: NgToastService ) { }
+
+  ngOnInit(): void {
+    // this.getAllProducts();
+  }
+
+//   openDialog() {
+//     this.dialog.open(DialogComponent, {
+//       width: '30%',
+//     }).afterClosed().subscribe(val => {
+//       if(val === 'save') {
+//         this.getAllProducts();
+//       }
+//     })
+//   }
+
+//   getAllProducts() {
+//     this.api.getProduct()
+//      .subscribe({
+//        next:(res)=>{
+//          this.dataSource = new MatTableDataSource(res);
+//          this.dataSource.paginator = this.paginator;
+//          this.dataSource.sort = this.sort;         
+//        },
+//        error:(err)=>{
+//          alert("Error while fetching the Records!")
+//        }
+//      })
+//   }
+
+//   editProduct(element: any) {
+//     this.dialog.open(DialogComponent, {
+//       width: '30%',
+//       data: element
+//     }).afterClosed().subscribe(val => {
+//       this.getAllProducts();
+//     })
+//   }
+
+//   deleteProduct(id: number) {
+//     this.api.deleteProduct(id)
+//      .subscribe({
+//        next: (res) => {
+//          this.toast.success({ detail: "Success Message", summary: "Product Delete successfully!", duration: 3000 })
+//          this.getAllProducts();
+//        },
+//        error: () => {
+//          this.toast.error({detail: "Error Message", summary: "Product Not Delete successfully!", duration: 3000})
+//        }
+//      })
+//  }
+
+//  applyFilter (event: Event) {
+//    const filterValue = (event.target as HTMLInputElement).value;
+//    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+//    if(this.dataSource.paginator) {
+//      this.dataSource.paginator.firstPage();
+//    }
+//  }
+
+}
---
+++ src/app/dialog/dialog.component.ts
@@ -0,0 +1,83 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from '../services/api.service';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { NgToastService } from 'ng-angular-popup';
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.css']
+})
+export class DialogComponent implements OnInit {
+
+  freshnessList = ["Brand New", "Second Hand", "Refurbished"];
+  productForm !: FormGroup;
+  actionBtn: string = "Save";
+
+  constructor(
+    private formBuilder: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public editData: any,
+    private api: ApiService,
+    private dialogRef: MatDialogRef<DialogComponent>,
+    private toast: NgToastService
+  ) { }
+
+  ngOnInit(): void {
+    this.productForm = this.formBuilder.group({
+      productName: ['', Validators.required],
+      category: ['', Validators.required],
+      freshness: ['', Validators.required],
+      price: ['', Validators.required],
+      comment: ['', Validators.required],
+      date: ['', Validators.required],
+    })
+
+    if (this.editData) {
+      this.actionBtn = "Update";
+      this.productForm.controls['productName'].setValue(this.editData.productName);
+      this.productForm.controls['category'].setValue(this.editData.category);
+      this.productForm.controls['freshness'].setValue(this.editData.freshness);
+      this.productForm.controls['price'].setValue(this.editData.price);
+      this.productForm.controls['comment'].setValue(this.editData.comment);
+      this.productForm.controls['date'].setValue(this.editData.date);
+    }
+
+  }
+
+  addProduct() {
+    if (!this.editData) {
+      if (this.productForm.valid) {
+        this.api.postProduct(this.productForm.value)
+          .subscribe({
+            next: (res: any) => {
+              this.toast.success({ detail: "Success Message", summary: "Product added successfully!", duration: 3000 })
+              this.productForm.reset();
+              this.dialogRef.close('save')
+            },
+            error: () => {
+              this.toast.error({ detail: "Error Message", summary: "Error while adding the product", duration: 3000 })
+            }
+          })
+      }
+    } else {
+      this.updateProduct()
+    }
+
+  }
+
+  updateProduct() {
+    this.api.putProduct(this.productForm.value, this.editData.id)
+     .subscribe({
+       next: (res) => {
+         this.toast.success({ detail: "Success Message", summary: "Product Successfully Update.", duration: 3000 })
+         this.productForm.reset();
+         this.dialogRef.close('Update')
+       },
+       error: (err) => {
+         this.toast.error({ detail: "Error Message", summary: "Sorry!, Somthing is wrong.", duration: 3000 })
+       }
+     })
+  }
+
+}
